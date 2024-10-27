f638ef088de0c77869a56d0b57803589de1cfdab
Vuxpn <vuphannguyen15102003@gmail.com>
2024-10-27 18:33:16 +0700

    up

--- src/devices/dto/createDevice.dto.ts
+++ src/devices/dto/createDevice.dto.ts
@@ -1,12 +1,7 @@
-import { IsString } from 'class-validator';
-
 export class CreateDeviceDto {
-  @IsString()
   deviceId: string;
 
-  @IsString()
   qrCodeId: string;
 
-  @IsString()
   qrCodeValue: string;
 }
