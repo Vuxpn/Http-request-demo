def76864fdf0f12a7718d0d7642c66f205167beb
Vuxpn <vuphannguyen15102003@gmail.com>
2024-10-27 21:33:14 +0700

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,7 @@ import { DevicesModule } from './devices/devices.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(`mongodb://localhost:27017/device`),
-    DevicesModule,
-  ],
+  imports: [],
   controllers: [],
   providers: [],
 })
